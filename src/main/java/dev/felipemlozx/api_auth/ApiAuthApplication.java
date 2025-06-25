8cad3437f893f79a58a5fe806b8fb5f170eeaf5a
Felipe Melo <felipemelozx@gmail.com>
2025-06-25 09:49:37 -0300

    feat: enable caching in ApiAuthApplication

--- src/main/java/dev/felipemlozx/api_auth/ApiAuthApplication.java
+++ src/main/java/dev/felipemlozx/api_auth/ApiAuthApplication.java
@@ -2,10 +2,12 @@ package dev.felipemlozx.api_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class ApiAuthApplication {
   public static void main(String[] args) {
     SpringApplication.run(ApiAuthApplication.class, args);
