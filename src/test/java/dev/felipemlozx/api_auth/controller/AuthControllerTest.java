2cf94fa557079b5ac54ace80a7b07952a40473c1
Felipe Melo <felipemelozx@gmail.com>
2025-07-07 16:47:12 -0300

    refactor: simplify AuthControllerTest class structure

--- src/test/java/dev/felipemlozx/api_auth/controller/AuthControllerTest.java
+++ src/test/java/dev/felipemlozx/api_auth/controller/AuthControllerTest.java
@@ -22,9 +22,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.any;
 
-public class AuthControllerTest {
+class AuthControllerTest {
 
   @InjectMocks
   private AuthController authController;
