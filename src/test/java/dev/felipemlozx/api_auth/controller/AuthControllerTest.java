09832d3f1c88b82e85a5923a7a651c3cff1f2bcb
Felipe Melo <felipemelozx@gmail.com>
2025-07-07 16:37:02 -0300

    test: add unit tests for AuthController to verify user registration and login functionality

---
+++ src/test/java/dev/felipemlozx/api_auth/controller/AuthControllerTest.java
@@ -0,0 +1,104 @@
+package dev.felipemlozx.api_auth.controller;
+
+import dev.felipemlozx.api_auth.controller.dto.CreateUserDto;
+import dev.felipemlozx.api_auth.controller.dto.LoginDTO;
+import dev.felipemlozx.api_auth.controller.dto.ResponseLoginDTO;
+import dev.felipemlozx.api_auth.services.AuthService;
+import dev.felipemlozx.api_auth.utils.ApiResponse;
+import jakarta.mail.MessagingException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+
+public class AuthControllerTest {
+
+  @InjectMocks
+  private AuthController authController;
+
+  @Mock
+  private AuthService authService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void shouldRegisterUserAndReturnSuccessResponse() throws MessagingException {
+    List<String> userList = new ArrayList<>();
+    CreateUserDto body = new CreateUserDto("test", "test@gmail.com", "Password!1");
+    when(authService.register(body)).thenReturn(userList);
+
+    ResponseEntity<ApiResponse<List<String>>> result = authController.register(body);
+    assertNotNull(result);
+
+    assertEquals(HttpStatus.CREATED, result.getStatusCode());
+    assertTrue(result.getBody().isSuccess());
+    assertEquals("User registered.", result.getBody().getMessage());
+    assertEquals("Verification email sent.", result.getBody().getData().get(0));
+  }
+
+  @Test
+  void shouldReturnBadRequestWhenRegisterFails() throws MessagingException {
+    List<String> fails = List.of("Email already exists");
+    CreateUserDto body = new CreateUserDto("test", "test@gmail.com", "Password!1");
+    when(authService.register(body)).thenReturn(fails);
+
+    ResponseEntity<ApiResponse<List<String>>> result = authController.register(body);
+    assertNotNull(result);
+    assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    assertFalse(result.getBody().isSuccess());
+    assertEquals(fails, result.getBody().getData());
+  }
+
+  @Test
+  void shouldLoginSuccessfully() {
+    LoginDTO loginDTO = new LoginDTO("test@gmail.com", "Password!1");
+    String token = "mocked-jwt-token";
+    when(authService.login(loginDTO)).thenReturn(token);
+
+    ResponseEntity<ApiResponse<ResponseLoginDTO>> response = authController.login(loginDTO);
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertTrue(response.getBody().isSuccess());
+    assertEquals("User logged in successfully", response.getBody().getMessage());
+    assertEquals(token, response.getBody().getData().token());
+    assertEquals(loginDTO.email(), response.getBody().getData().email());
+  }
+
+  @Test
+  void shouldReturnBadRequestWhenVerifyEmailFails() {
+    String token = "invalid-token";
+    when(authService.verifyEmailToken(token)).thenReturn(false);
+
+    ResponseEntity<ApiResponse<String>> response = authController.verifyEmail(token);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertFalse(response.getBody().isSuccess());
+    assertEquals("Link invalid.", response.getBody().getMessage());
+  }
+
+  @Test
+  void shouldReturnOkWhenVerifyEmailSucceeds() {
+    String token = "valid-token";
+    when(authService.verifyEmailToken(token)).thenReturn(true);
+
+    ResponseEntity<ApiResponse<String>> response = authController.verifyEmail(token);
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+}
