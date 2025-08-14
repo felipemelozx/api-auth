5c3174e04c63235f8e8e54620cefffa13acb669d
Felipe Melo <felipemelozx@gmail.com>
2025-08-14 16:23:21 -0300

    feat: add controller to demonstrat authorization functionality

---
+++ src/main/java/dev/felipemlozx/api_auth/controller/ClubController.java
@@ -0,0 +1,37 @@
+package dev.felipemlozx.api_auth.controller;
+
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.felipemlozx.api_auth.utils.ApiResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("club/secret")
+public class ClubController {
+  private List<String> phrasesList = List.of(
+    "Acredite no seu potencial e svá além.",
+    "Cada desafio é uma oportunidade de crescimento.",
+    "Persistência é o caminho para o sucesso.",
+    "Faça hoje o que outros não querem para conquistar amanhã.",
+    "Transforme obstáculos em degraus para o seu progresso.",
+    "O esforço de hoje constrói a vitória de amanhã.",
+    "A disciplina supera o talento quando o talento não se esforça.",
+    "Sonhe grande, comece pequeno, aja agora.",
+    "Não espere por oportunidades, crie-as.",
+    "Seu único limite é você mesmo."
+);
+  public ClubController() {}
+
+  @GetMapping
+  public ResponseEntity<ApiResponse<String>> getPhrases(){
+    int index = new Random().nextInt(phrasesList.size());
+    String mensagem = phrasesList.get(index);
+    return ResponseEntity.ok().body(ApiResponse.success("You are authorization",mensagem));
+  }
+}
